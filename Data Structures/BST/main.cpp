e7ef4b2045496083aae5a65c69883ddcb3a5fc64
Moisés Fernández <moy-881@hotmail.com>
2018-10-19 12:13:06 -0500

    Modified tests

--- Data Structures/BST/main.cpp
+++ Data Structures/BST/main.cpp
@@ -7,25 +7,39 @@ using namespace std;
 
 int main () {
     BST miArbol;
-    miArbol.add(11);
-    miArbol.add(6);
-    miArbol.add(19);
-    miArbol.add(4);
-    miArbol.add(8);
-    miArbol.add(17);
-    miArbol.add(43);
-    miArbol.add(5);
-    miArbol.add(10);
-    miArbol.add(31);
-    miArbol.add(49);
+    // miArbol.add(11);
+    // miArbol.add(6);
+    // miArbol.add(19);
+    // miArbol.add(4);
+    // miArbol.add(8);
+    // miArbol.add(17);
+    // miArbol.add(43);
+    // miArbol.add(5);
+    // miArbol.add(10);
+    // miArbol.add(31);
+    // miArbol.add(49);
     //miArbol.print(1);
     // cout << endl;
     // miArbol.print(2);
     // cout << endl;
     // miArbol.print(3);
     // cout << endl;
+    // miArbol.add(18);
+    miArbol.add(5);
+    miArbol.add(10);
+    miArbol.add(15);
+    miArbol.add(20);
+    miArbol.add(2);
+    miArbol.add(50);
+    miArbol.add(33);
 
-    cout << "11 6 19 4 8 17 43 5 10 31 49" << endl;
+    // cout << "11 6 19 4 8 17 43 5 10 31 49" << endl;
+    cout << "5 10 15 20 2 50 33" << endl;
+    // miArbol.print(1);
+    // cout << endl;
+    // miArbol.mirror();
+    // miArbol.print(1);
+    // cout << endl;
 
     // if (miArbol.search(100)) {
     //     cout << "SI ENCONTRE EL 100" << endl;
@@ -67,22 +81,43 @@ int main () {
     // miArbol.print(1);
     // cout << endl;
 
-    BST arbol2(miArbol); //BST arbol2 = BST(miArbol);
+    // BST arbol2(miArbol); //BST arbol2 = BST(miArbol);
 
-    if (miArbol == arbol2)
-        cout << "iguales" << endl;
+    // if (miArbol == arbol2)
+    //     cout << "iguales" << endl;
+    // else
+    //     cout << "diferentes" << endl;
+
+    // cout << "maxWidth de arbol1 = " << miArbol.maxWidth() << endl;
+    // cout << "maxWidth de arbol2 = " << arbol2.maxWidth() << endl;
+
+    // cout << "~arbol 2~" << endl;
+    // arbol2.print(1);
+    // arbol2.mirror();
+    // cout << endl << "~inverso de arbol2~" << endl;
+    // arbol2.print(1);
+    // cout << endl <<  "nearestRelative de 31 y 49 en el arbol2 = " << arbol2.nearestRelative(31, 49) << endl;
+
+    // cout << "~Menores~" << endl;
+    cout << "Menores a 5 = " << miArbol.howManyAreSmallerThanMe(5) << endl;
+    cout << "Menores a 2 = " << miArbol.howManyAreSmallerThanMe(2) << endl;
+    cout << "Menores a 6 = " << miArbol.howManyAreSmallerThanMe(6) << endl;
+    cout << "Menores a 16 = " << miArbol.howManyAreSmallerThanMe(16) << endl;
+    cout << "Menores a 22 = " << miArbol.howManyAreSmallerThanMe(22) << endl;
+    cout << "Menores a 45 = " << miArbol.howManyAreSmallerThanMe(45) << endl;
+    cout << "Menores a 51 = " << miArbol.howManyAreSmallerThanMe(51) << endl;
+
+    if (miArbol.check())
+        cout << "It's a BST!" << endl;
     else
-        cout << "diferentes" << endl;
+        cout << "It's not a BST!" << endl;
+    
+    miArbol.mirror();
 
-    cout << "maxWidth de arbol1 = " << miArbol.maxWidth() << endl;
-    cout << "maxWidth de arbol2 = " << arbol2.maxWidth() << endl;
-
-    cout << "~arbol 2~" << endl;
-    arbol2.print(1);
-    arbol2.mirror();
-    cout << endl << "~inverso de arbol2~" << endl;
-    arbol2.print(1);
-    cout << endl <<  "nearestRelative de 31 y 49 en el arbol2 = " << arbol2.nearestRelative(31, 49) << endl;
+    if (miArbol.check())
+        cout << "It's a BST!" << endl;
+    else
+        cout << "It's not a BST!" << endl;
 
     return 0;
 }
